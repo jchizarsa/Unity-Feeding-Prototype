b03dccd8520a21eaab6730e09b7d36a7c4e42fad
Jc <jchizarsa@gmail.com>
2022-05-21 23:43:05 -0700

    Basic level design & Player input
    Change log:
    - Added some objects to the scene
    -- Player, 3 animals, 1 food
    - Wrote basic player input code.
    
    AP:
    - Prepare for projectile food!

---
+++ Prototype 2/Assets/_Project/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float horizontalInput;
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float xRange = 10.0f;
+    private Rigidbody rb; // Used for velocity based movement
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); // Used for velocity based movement
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the horizontal input
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Transform.translate based movement
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        // Velocity based movement application
+        // rb.velocity = new Vector3(horizontalInput, 0, 0) * 10;
+
+        /// <summary> -- Clamp the position of the player within the xRange -- </summary>
+        if(transform.position.x < -xRange){
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if(transform.position.x > xRange){
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        //----------------------------------------------------------------------------------
+    }
+}
